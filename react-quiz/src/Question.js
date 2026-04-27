774502038ea007bed7aa76316da94980890349d7
Ephrem216 <156383015+Ephrem216@users.noreply.github.com>
2026-04-27 17:08:32 +0300

    displaying questions

--- react-quiz/src/Question.js
+++
@@ -1,5 +0,0 @@
-function Question() {
-  return <div>Hello from questions world</div>;
-}
-
-export default Question;
--- react-quiz/src/components/App.js
+++ react-quiz/src/components/App.js
@@ -9,6 +9,7 @@ import Question from "./Question";
 const initialState = {
   questions: [],
   status: "loading",
+  index: 0,
 };
 
 function reducer(state, action) {
@@ -31,7 +32,10 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index }, dispatch] = useReducer(
+    reducer,
+    initialState,
+  );
 
   const numQuestions = questions.length;
   useEffect(function () {
@@ -51,7 +55,7 @@ export default function App() {
         {status === "ready" && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === "active" && <Question />}
+        {status === "active" && <Question question={questions[index]} />}
       </Main>
     </div>
   );
