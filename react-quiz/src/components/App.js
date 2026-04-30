d6999211c0be35644b3175adecfd8fc28630b733
Ephrem216 <156383015+Ephrem216@users.noreply.github.com>
2026-05-01 02:48:20 +0300

    restarting a quiz

--- react-quiz/src/components/App.js
+++ react-quiz/src/components/App.js
@@ -51,6 +51,12 @@ function reducer(state, action) {
         highscore:
           state.points > state.highscore ? state.points : state.highscore,
       };
+    case "restart":
+      return {
+        ...initialState,
+        questions: state.questions,
+        status: "ready",
+      };
     default:
       throw new Error("Unknon action");
   }
@@ -110,6 +116,7 @@ export default function App() {
             points={points}
             maxPossiblePoints={maxPossiblePoints}
             highscore={highscore}
+            dispatch={dispatch}
           />
         )}
       </Main>
