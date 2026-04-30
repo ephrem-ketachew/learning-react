9cf79abce58ce5d74124f4da2abf0d329f26ba6e
Ephrem216 <156383015+Ephrem216@users.noreply.github.com>
2026-05-01 02:21:53 +0300

    finishing a quiz

---
+++ react-quiz/src/components/FinishScreen.js
@@ -0,0 +1,23 @@
+function FinishScreen({ points, maxPossiblePoints, highscore }) {
+  const percentage = (points / maxPossiblePoints) * 100;
+
+  let emoji;
+  if (percentage === 100) emoji = "🎖️";
+  else if (percentage >= 80) emoji = "🎉";
+  else if (percentage >= 50) emoji = "☺️";
+  else if (percentage > 0) emoji = "😕";
+  else emoji = "🤕";
+
+  return (
+    <>
+      <p className="result">
+        <span>{emoji}</span>
+        You scored <strong>{points}</strong> out of {maxPossiblePoints} (
+        {Math.ceil(percentage)}%)
+      </p>
+      <p className="highscore">(Highscore: {highscore} points)</p>
+    </>
+  );
+}
+
+export default FinishScreen;
